1840b1c8d45bc9a1aa912db0bb4f3f74939b506d
PiotrFedyna <167698@stud.prz.edu.pl>
2022-03-22 10:58:14 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("H "+ "\n"+"e"+"\n"+"l"+"\n"+"l"+"\n"+"W"+"\n"+"o"+"\n"+"r"+"\n"+"l"+"\n"+"d");
+    }
+}
